9031e481d442178ecd1b171afdddbb0aeeb3621f
leandro-ikehara <leandro.ikehara@gmail.com>
2025-05-02 11:37:12 -0300

    Recursos e novas rotas

--- main.go
+++ main.go
@@ -2,24 +2,53 @@ package main
 
 import (
 	"pizzaria/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
+
+var pizzas = []models.Pizza{
+	{ID: 1, Nome: "Margherita", Preco: 25.50},
+	{ID: 2, Nome: "Pepperoni", Preco: 30.00},
+	{ID: 3, Nome: "Quattro Formaggi", Preco: 35.00},
+}
 
 func main() {
 	//nomePizzaria := "Go Pizza"
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", postPizzas)
+	router.GET("/pizzas/:id", getPizzasByID)
 	router.Run()
 }
 
 func getPizzas(c *gin.Context) {
-	var pizzas = []models.Pizza{
-		{ID: 1, Nome: "Margherita", Preco: 25.50},
-		{ID: 2, Nome: "Pepperoni", Preco: 30.00},
-		{ID: 3, Nome: "Quattro Formaggi", Preco: 35.00},
-	}
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
 	})
+}
+
+func getPizzasByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "ID must be a number"})
+		return
+	}
+	for _, pizza := range pizzas {
+		if pizza.ID == id {
+			c.JSON(200, pizza)
+			return
+		}
+	}
+	c.JSON(404, gin.H{"error": "Pizza not found"})
+}
+
+func postPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	pizzas = append(pizzas, newPizza)
 }
